af8ea2bc319157e4a00647824cf4d33e545f86ae
unknown <sajanneyveli@gmail.com>
2024-11-16 11:35:03 +0530

    updated

--- prog/n_queen.py
+++ prog/n_queen.py
@@ -1,59 +1,38 @@
-def is_safe(board, row, col, n):
-    # Check for queen in the same row to the left
+def is_safe(board, row,col,n):
     for i in range(col):
-        if board[row][i] == 1:
+        if board[row][i]==1:
             return False
-
-    # Check upper diagonal to the left
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
+    for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
+        if board[i][j]==1:
             return False
-
-    # Check lower diagonal to the left
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
+    for i,j in zip(range(row,n,1),range(col,-1,-1)):
+        if board[i][j]==1:
             return False
-
     return True
 
-
-def solve_n_queen_util(board, col, n):
-    # Base condition: All queens are placed
-    if col >= n:
+def solve_n_queen_util(board,col,n):
+    if col>=n:
         return True
-
-    # Place queen in all rows one by one
     for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1  # Place queen
+        if is_safe(board,i,col,n):
+            board[i][col]=1
+            if solve_n_queen_util(board,col+1,n):
+                return  True
 
-            # Recur to place the rest of the queens
-            if solve_n_queen_util(board, col + 1, n):
-                return True
-
-            # Backtrack: Remove the queen
-            board[i][col] = 0
-
+        board[i][col]=0 #backtracking
     return False
 
-
-def solve_n_queens(n):
+def solve_n_queen(n):   #initilize the chess board
     board = [[0 for _ in range(n)] for _ in range(n)]
-
-    if not solve_n_queen_util(board, 0, n):
-        print("Solution does not exist.")
+    if not solve_n_queen_util(board,0,n):
+        print("Solution Does not exists")
         return False
 
     print_board(board)
     return True
-
-
 def print_board(board):
     for row in board:
-        print(" ".join("Q" if x == 1 else "." for x in row))
+        print(" ".join("Q" if x==1 else "." for x in row))
     print("\n")
-
-
-# Input and function call
-n = int(input("Enter value of n: "))
-solve_n_queens(n)
+n=int(input("Enter The NxN len for chess board:"))
+solve_n_queen(n)
